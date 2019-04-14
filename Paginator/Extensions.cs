afc752b11ce4e967164ea0d974ba21169049c16e
devtimmy <timothy.macharia@outlook.com>
2019-04-14 03:07:29 +0300

    refactors extension methods

--- Paginator/Extensions.cs
+++ Paginator/Extensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
@@ -7,22 +6,9 @@ namespace Paginator
 {
     public static class Extensions
     {
-        /// <summary>
-        /// Checks if a <see cref="string"/> is valid by whether it's empty, null 
-        /// or whitespace.
-        /// </summary>
-        /// <param name="s">Text to evaluate.</param>
-        /// <returns>
-        /// <see cref="true"/> or <see cref="false"/>
-        /// </returns>
-        public static bool IsValid(this string s)
-        {
-            if (!string.IsNullOrWhiteSpace(s))
-            {
-                if (s.Length > 0)
-                {
-                    return true;
-                }
+        public static bool IsValid(this string s) {
+            if (!string.IsNullOrWhiteSpace(s)) {
+                return s.Length > 0;
             }
             return false;
         }
@@ -32,10 +18,8 @@ namespace Paginator
                 return Regex.IsMatch(s, $"({q})", RegexOptions.IgnoreCase);
             return false;
         }
-        public static string CastToString(this object obj)
-        {
-            if(obj != null)
-            {
+        public static string CastToString(this object obj) {
+            if(obj != null) {
                 return obj.ToString();
             }
             return string.Empty;
